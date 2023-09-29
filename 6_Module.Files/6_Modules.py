83462eacb4004d575e66b7a6f3ec4a16b268f4cf
New GitHub (in progress) <108394864+MikalaiRozum@users.noreply.github.com>
2023-09-29 18:15:49 +0400

    6_Modules

---
+++ 6_Module.Files/6_Modules.py
@@ -0,0 +1,151 @@
+import os
+import datetime
+from Functions import normalize_text, capitalize_sentences
+
+script_directory = os.path.dirname(os.path.abspath(__file__))
+NEWSFEED_PATH = os.path.join(script_directory, 'newsfeed.txt')
+
+
+class ContentPublisher:
+    def __init__(self, content_type, text, body=''):
+        self.type = content_type
+        self.text = text
+        self.body = body
+
+    def write_to_file(self):
+        try:
+            with open(NEWSFEED_PATH, 'a') as f:
+                f.write(self.body + '\n\n')
+            print('INFO. Successful. New publication added.')
+            return True
+        except Exception as e:
+            print(f'INFO. Fail. New publication was not added. Error: {e}')
+            return False
+
+    def publish(self):
+        try:
+            body_creator = {
+                "NEWS": self.create_news_body,
+                "PRIVATE AD": self.create_private_ad_body,
+                "PERSONAL BLOG": self.create_personal_blog_body
+            }
+            self.body = body_creator.get(self.type.upper(), self.default_body)()
+        except Exception as e:
+            print(f'{e}')
+            return False
+
+        return self.write_to_file() if self.body else False
+
+    @staticmethod
+    def default_body():
+        return ''
+
+    def create_news_body(self):
+        current_time = datetime.datetime.now().strftime("%d/%m/%Y, %H:%M")
+        max_retries = 3
+        for _ in range(max_retries):
+            city = input('City: ').strip().capitalize()
+            if city.isalpha():
+                break
+            else:
+                print("ERROR: City name should contain only alphabetic characters. Please try again.")
+        else:
+            print("Reached maximum number of retries. Exiting.")
+            return None
+
+        return f'News -------------------------\n{self.text}\n{city}, {current_time}'
+
+    def create_private_ad_body(self):
+        days_left, expiration_date = self.calculate_time_until_expiration()
+        return f'Private ad -------------------\n{self.text}\nActual until: {expiration_date.strftime("%d/%m/%Y")}, {days_left} days left'
+
+    @staticmethod
+    def calculate_time_until_expiration():
+        try:
+            expiration_date_input = input('Please input "Actual until" date in format d/m/Y: ')
+            expiration_date = datetime.datetime.strptime(expiration_date_input, "%d/%m/%Y")
+            current_date = datetime.datetime.now()
+
+            if expiration_date < current_date:
+                raise ValueError("Expiration date cannot be in the past.")
+
+            days_left = (expiration_date - current_date).days
+            return str(days_left), expiration_date
+        except Exception as e:
+            print(f'ERROR: Enter a valid future date in the following format: d/m/Y. Error: {e}')
+            return '', None
+
+    def create_personal_blog_body(self):
+        author = input('Author: ').strip().capitalize()
+        return f'Personal blog ----------------\n{self.text}\nAuthor: {author}'
+
+
+class InputHandler:
+    @staticmethod
+    def get_text_input():
+        # Method to get user input for text creation or loading from a file
+        choice = input('Choose an option (Create text / Load text): ').strip().lower()
+
+        if choice == 'create text':
+            text = input('Enter the text: ')
+        elif choice == 'load text':
+            text = InputHandler.load_text_from_file()
+        else:
+            print('Invalid option. Please choose either "Create text" or "Load text".')
+            return None
+
+        return text
+
+    @staticmethod
+    def load_text_from_file():
+        # Method to load text from a file, either from a default folder or user provided file path
+        load_option = input('Choose an option (Select from default folder / Input file path): ').strip().lower()
+
+        if load_option == 'select from default folder':
+            file_path = os.path.join(script_directory, 'text.txt')
+        elif load_option == 'input file path':
+            file_path = input('Enter the file path: ').strip()
+        else:
+            print('Invalid option. Please choose either "Select from default folder" or "Input file path".')
+            return None
+
+        try:
+            with open(file_path, 'r') as file:
+                text = file.read()
+
+            # If file successfully processed, attempt to remove it
+            remove_file = input('Do you want to remove the processed file? (yes/no): ').strip().lower()
+            if remove_file == 'yes':
+                os.remove(file_path)
+                print(f'File {file_path} removed successfully.')
+            elif remove_file != 'no':
+                print('Invalid option. Please choose either "yes" or "no".')
+
+            return text
+        except FileNotFoundError:
+            print(f'ERROR: File not found at {file_path}')
+        except PermissionError:
+            print(f'ERROR: Permission denied. Make sure you have the necessary permissions to read the file at {file_path}')
+        except Exception as e:
+            print(f'Error loading text from file: {e}')
+
+        return None
+
+
+if __name__ == "__main__":
+    allowed_content_types = ["News", "Private ad", "Personal blog"]
+
+    content_type = input('Choose what you want to publish (News, Private ad, Personal blog): ').strip().capitalize()
+
+    if content_type not in allowed_content_types:
+        print('ERROR: Incorrect publishing type. Available types: News, Private ad, Personal blog')
+    else:
+        text = InputHandler.get_text_input()
+
+        if text is not None:
+            # Applying of case normalization functionality form Homework 3/4
+            normalized_text = normalize_text(text)
+            normalized_sentences = capitalize_sentences(normalized_text)
+            final_text = ' '.join(normalized_sentences)
+            new_publication_object = ContentPublisher(content_type, final_text)
+            new_publication_object.publish()
